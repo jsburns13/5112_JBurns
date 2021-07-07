61a3bf5030e45791de10e74164c6510d812f3e14
jsburns13 <jsburns13@gmail.com>
2021-07-06 23:33:14 -0700

    Commit for nfhs_edu sub-set

--- Week2_Assgn_Script.R
+++ Week2_Assgn_Script.R
@@ -1,19 +1,23 @@
 library(tidyverse)
 library(haven)
 
-#read in nfhs data
-nfhs <- read_dta("IAHR52FL(2).dta")
+#read in\ nfhs data
+nfhs <- read_dta("IAHR52FL.dta")
 
-view(select(nfhs, hv107))
+test_nfhs <- nfhs %>%
+  sample_frac(0.1)
 
-nfhs_wk2 <- select(nfhs, hhid:hv208, hv270)
+nfhs_wk2 <- select(test_nfhs, hhid:hv208, hv270)
 
-nfhs_edu <- select(nfhs, hhid, hv002, hv003, hv108_01:hv109_01, -hv109_01)
+nfhs_edu <- select(test_nfhs, hhid, hv002, hv003, hv108_01:hv109_01, -hv109_01)
 
-nfhs_fem <- select(nfhs, hhid, ha0_01:ha6_11)
+nfhs_fem <- select(test_nfhs, hhid, ha0_01:ha6_11)
 
-nfhs_male <- select(nfhs, hhid, hb0_01:hb6_18)
+nfhs_male <- select(test_nfhs, hhid, hb0_01:hb6_18)
 
-as_tibble(nfhs_edu) %>%
-  gather(hv108_01:hv108_35, key = "member", value = "year of education") %>%
-  separate(member, into = c("init_code", "roster number"), sep = "_")
+nfhs_edu <- select(nfhs, hhid, hv002, hv003, hv108_01:hv108_35) %>%
+  gather(hv108_01:hv108_35, key = "member", value = "years_of_education") %>%
+  separate(member, into = c("init_code", "roster_number"), sep = "_") %>%
+  select(hhid, roster_number, years_of_education) %>%
+  rename(household_id = hhid) %>%
+  drop_na(years_of_education)
