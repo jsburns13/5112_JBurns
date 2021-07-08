5c591391040699b720090cbaedbae7842db9f43a
jsburns13 <jsburns13@gmail.com>
2021-07-08 12:46:09 -0700

    Revised logic for nfhs_edu to properly capture roster_number

--- Week 2/Week2_Assgn_Script.R
+++ Week 2/Week2_Assgn_Script.R
@@ -5,8 +5,8 @@ library(haven)
 nfhs <- read_dta("IAHR52FL.dta")
 
 # cuts data set down to 10% for testing purposes; comment out for prod
-nfhs <- nfhs %>%
-  sample_frac(0.1)
+# nfhs <- nfhs %>%
+#   sample_frac(0.1)
 
 # subset for household data
 nfhs_wk2 <- select(nfhs, hhid:hv208, hv270) %>%
@@ -14,25 +14,66 @@ nfhs_wk2 <- select(nfhs, hhid:hv208, hv270) %>%
   rename(roster_number = hv003)
 
 # subset education data by household member
-nfhs_edu <- select(nfhs, hhid, hv003, hv108_01:hv108_35) %>%
-  gather(hv108_01:hv108_35, key = "member", value = "years_of_education") %>%
-  separate(member, into = c("init_code", "roster_number"), sep = "_") %>%
-  select(hhid, roster_number, years_of_education) %>%
-  drop_na(years_of_education) %>%
-# need to convert roster_number to allow joins later on
-  transform(roster_number = as.numeric(roster_number))
+# 71 variables created in select statement
+nfhs_edu <- select(nfhs, hhid, hvidx_01:hvidx_35, hv108_01:hv108_35)
+
+### tidy education data for final joins
+### This code technically functions, but in the process of un-pivoting and re-pivoting,
+### it maxes out memory and ultimately aborts unless it's being run on a small
+### test data set. Continue past this commented code for a working solution.
+
+# nfhs_edu <- nfhs_edu %>%
+# # gather education and roster_number into separate single columns
+#   gather(hv108_01:hv108_35, key = "edu_key", value = "years_of_education") %>%
+#   gather(hvidx_01:hvidx_35, key = "id_key", value = "roster_number") %>%
+# # last 2 digits of initial column names are unique IDs that can be matched on
+#   separate(edu_key, into = c("var_code", "edu_id"), sep = "_") %>%
+#   separate(id_key, into = c("var_code2", "id_id"), sep = "_") %>%
+# # T/F check identifies duplicated rows and filters them out
+#   mutate(member_check = (edu_id == id_id)) %>%
+#   filter(member_check == TRUE) %>%
+#   select(hhid, roster_number, years_of_education) %>%
+#   drop_na(years_of_education) %>%
+# # need to convert roster_number to allow joins later on
+#   transform(roster_number = as.numeric(roster_number))
+
+### EXTRA CONTEXT FOR CODE; NOT REQUIRED TO UNDERSTAND CODE
+### Since gathering and separating each set of data, as above, would multiply total row counts,
+### potentially crashing my computer, I created separate data frames that gather and separate
+### the variables, and then join everything together based on the roster_number.
+### For similar reasons, I am not referencing the generic data frame above (nfhs_edu) in the piping below to
+### limit the number of data frames loading in R and help prevent maxing out my memory.
+### It is possible to do this all in one data frame, but it exhausts my memory and aborts the program when
+### running on the full data set. It works with the 10% subset, though.
+
+### SUBSET EDUCATION DATA
+
+# isolate roster number and tidy data frame
+nfhs_edu_id <- select(nfhs, hhid, hvidx_01:hvidx_35) %>%
+  gather(hvidx_01:hvidx_35, key = "id_key", value = "roster_number") %>%
+  separate(id_key, into = c("var_code", "edu_id"), sep = "_") %>%
+  select(hhid, edu_id, roster_number) %>%
+  drop_na(roster_number)
+
+# isolate years of education and tidy data frame
+nfhs_edu_edu <- select(nfhs, hhid, hv108_01:hv108_35) %>%
+  gather(hv108_01:hv108_35, key = "edu_key", value = "years_of_education") %>%
+  separate(edu_key, into = c("var_code", "edu_id"), sep = "_") %>%
+  select(hhid, edu_id, years_of_education) %>%
+  drop_na(years_of_education)
+
+# join data back together
+nfhs_edu <- nfhs_edu_id %>%
+  full_join(nfhs_edu_edu) %>%
+  select(-edu_id)
+
+# remove intermediate data frames to keep environment tidy
+rm(nfhs_edu_id, nfhs_edu_edu)
+
+### SUBSET FEMALE DATA
 
 # full dataset of bio data for female household members
 # nfhs_fem <- select(nfhs, hhid, ha0_01:ha6_11)
-
-### EXTRA CONTEXT FOR CODE; NOT REQUIRED TO UNDERSTAND CODE
-### Since gathering and separating each set of data would duplicate and triplicate total row counts,
-### potentially crashing my computer, I created three separate data frames that gather and separate
-### the variables, and then join everything together based on the roster_number.
-### For similar reasons, I am not referencing the data frame above (nfhs_fem) in the piping below to
-### limit the number of data frames loading in R and help prevent maxing out my memory.
-### It is possible to do this all in one data frame, but it would take extra effort to remove duplicates
-### and remove rows that associated household members with data from other household members.
 
 # isolate roster number and tidy data frame
 nfhs_fem_id <- select(nfhs, hhid, ha0_01:ha6_11) %>%
@@ -82,6 +123,8 @@ nfhs_fem_biom <- nfhs_fem_id %>%
 
 # remove intermediate data frames to keep environment tidy
 rm(nfhs_fem_age, nfhs_fem_height, nfhs_fem_weight, nfhs_fem_id)
+
+### SUBSET MALE DATA
 
 ### full dataset of bio date for male household members
 # nfhs_male <- select(nfhs, hhid, hb0_01:hb6_18)
@@ -134,6 +177,8 @@ nfhs_male_biom <- nfhs_male_id %>%
 
 # remove intermediate data frames to keep environment tidy
 rm(nfhs_male_age, nfhs_male_height, nfhs_male_weight, nfhs_male_id)
+
+### COMBINE DATA TO CREATE FINAL, TIDY TABLE
 
 # merge female and male data sets first
 nfhs_indiv <- nfhs_male_biom %>%
